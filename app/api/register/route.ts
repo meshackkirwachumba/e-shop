5bbae7d7a28583c7017fd097578671b37a228730
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-01 16:25:03 +0300

    created register route

---
+++ app/api/register/route.ts
@@ -0,0 +1,24 @@
+import bcrypt from "bcrypt";
+import prisma from "@/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  try {
+    const { name, email, password } = body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
