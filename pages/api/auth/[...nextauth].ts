c5a14f96cce545d0e4724fadea934986ad535392
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-01 15:41:17 +0300

    [...nextauth].ts file and global prisma client

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,67 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "@/libs/prismadb";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        //check if user has provided email and password
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Invalid email or password!");
+        }
+
+        //check if the user exists in the db
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        // if the user does not exists
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid email or password");
+        }
+
+        // check if password passed matches the one in db
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        // if password is incorrect
+        if (!isCorrectPassword) {
+          throw new Error("Invalid email or password");
+        }
+
+        // if password is correct
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
