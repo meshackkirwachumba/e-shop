e393defd041d2bd65c4c89f97989fcf58d14626c
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-01 11:22:10 +0300

    reusable input component

---
+++ app/components/inputs/Input.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+}
+
+const Input = ({
+  id,
+  label,
+  type,
+  disabled,
+  required,
+  register,
+  errors,
+}: InputProps) => {
+  return (
+    <div className="w-full relative">
+      <input
+        autoComplete="off"
+        id={id}
+        disabled={disabled}
+        {...register(id, { required })}
+        placeholder=" "
+        type={type}
+        className={`
+         peer
+         w-full
+         p-4
+         pt-6
+         outline-none
+         bg-white
+         font-light
+         border-2
+         rounded-md
+         transition
+         disabled:opacity-70
+         disabled:cursor-not-allowed
+         ${errors[id] ? "border-rose-400" : "border-slate-300"}
+         ${errors[id] ? "focus:border-rose-400" : "focus:border-slate-300"}
+         `}
+      />
+      <label
+        htmlFor={id}
+        className="
+          absolute
+          top-5
+          left-4
+          cursor-text
+          text-md
+          duration-150
+          transform
+          -translate-y-3
+          z-10
+          origin-[0]
+          peer-placeholder-shown:scale-100
+          peer-placeholder-shown:translate-y-0
+          peer-focus:scale-75
+          peer-focus:translate-y-4
+
+
+        "
+      >
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default Input;
