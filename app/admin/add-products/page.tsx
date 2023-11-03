31f7c9d466d2d1bb01ef6e7af77fde02f7cb8b47
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-03 21:26:09 +0300

    admin and nested routes with a default layout

---
+++ app/admin/add-products/page.tsx
@@ -0,0 +1,5 @@
+const AddProduct = () => {
+  return <div>Add Products</div>;
+};
+
+export default AddProduct;
---
+++ libs/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyuJaCX_mbXM8R6cfVlS5q9IoFipmTOwE",
+  authDomain: "e-shop-vid-53f14.firebaseapp.com",
+  projectId: "e-shop-vid-53f14",
+  storageBucket: "e-shop-vid-53f14.appspot.com",
+  messagingSenderId: "228212404785",
+  appId: "1:228212404785:web:a526a98504881b12c2db10",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
