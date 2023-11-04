07288c17178caec95dacd2d2905e6568ffedaba2
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-04 16:52:59 +0300

    add product

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,20 +1,48 @@
 "use client";
 
+import Button from "@/app/components/Button";
 import Heading from "@/app/components/Heading";
 import CategoryInput from "@/app/components/inputs/CategoryInput";
 import CustomCheckbox from "@/app/components/inputs/CustomCheckbox";
 import Input from "@/app/components/inputs/Input";
+import SelectColor from "@/app/components/inputs/SelectColor";
 import TextArea from "@/app/components/inputs/TextArea";
+import firebaseApp from "@/libs/firebase";
 import { SafeUser } from "@/types";
 import { categories } from "@/utils/Categories";
-import { useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { colors } from "@/utils/Colors";
+import axios from "axios";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+
+export type ImageType = {
+  color: string;
+  colorCode: string;
+  image: File | null;
+};
+export type UploadedImageType = {
+  color: string;
+  colorCode: string;
+  image: string;
+};
 
 interface AddProductFormProps {
   currentUser: SafeUser | null | undefined;
 }
 const AddProductForm = ({ currentUser }: AddProductFormProps) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [images, setImages] = useState<ImageType[] | null>(null);
+  const [isProductCreated, setIsProductCreated] = useState(false);
+
+  const router = useRouter();
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -43,7 +71,134 @@ const AddProductForm = ({ currentUser }: AddProductFormProps) => {
     },
   });
 
+  useEffect(() => {
+    setCustomValue("images", images);
+  }, [images]);
+
   const category = watch("category");
+
+  useEffect(() => {
+    if (isProductCreated) {
+      reset();
+      setImages(null);
+      setIsProductCreated(false);
+    }
+  }, [isProductCreated]);
+
+  //upload product to db
+  const onsubmitData: SubmitHandler<FieldValues> = async (data) => {
+    //upload iamges to firebase
+    setIsLoading(true);
+    let uploadedImages: UploadedImageType[] = [];
+
+    if (!data.category) {
+      setIsLoading(false);
+      return toast.error("category is not selected");
+    }
+
+    if (!data.images || data.images.length === 0) {
+      setIsLoading(false);
+      toast.error("No selected image");
+    }
+
+    const handleImageUploads = async () => {
+      toast("Creating product, please wait...");
+
+      try {
+        for (const item of data.images) {
+          if (item.image) {
+            const fileName = new Date().getTime() + "-" + item.image.name;
+            const storage = getStorage(firebaseApp);
+            const storageRef = ref(storage, `products/${fileName}`);
+            const uploadTask = uploadBytesResumable(storageRef, item.image);
+
+            await new Promise<void>((resolve, reject) => {
+              uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                  // Observe state change events such as progress, pause, and resume
+                  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                  const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                  console.log("Upload is " + progress + "% done");
+                  switch (snapshot.state) {
+                    case "paused":
+                      console.log("Upload is paused");
+                      break;
+                    case "running":
+                      console.log("Upload is running");
+                      break;
+                  }
+                },
+                (error) => {
+                  // Handle unsuccessful uploads
+                  console.log("Error uploading image");
+                  reject(error);
+                },
+                () => {
+                  // Handle successful uploads on complete
+                  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                  getDownloadURL(uploadTask.snapshot.ref)
+                    .then((downloadURL: string) => {
+                      uploadedImages.push({ ...item, image: downloadURL });
+                      console.log("File available at", downloadURL);
+                      resolve();
+                    })
+                    .catch((error) => {
+                      console.log("Error getting the downloadURL", error);
+                      reject(error);
+                    });
+                }
+              );
+            });
+          }
+        }
+      } catch (error) {
+        setIsLoading(false);
+        console.log("Error handling image uploads", error);
+        return toast.error("Error handling image uploads");
+      }
+    };
+
+    await handleImageUploads();
+
+    const productData = { ...data, images: uploadedImages };
+
+    //save product in the db
+    axios
+      .post("/api/product", productData)
+      .then(() => {
+        toast.success("Product created");
+        setIsProductCreated(true);
+        router.refresh();
+      })
+      .catch((error) => {
+        toast.error("Something went wrong when saving product in db");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const addImageToState = useCallback((imgObject: ImageType) => {
+    setImages((prevImagesArray) => {
+      if (!prevImagesArray) {
+        return [imgObject];
+      }
+      return [...prevImagesArray, imgObject];
+    });
+  }, []);
+  const removeImageFromState = useCallback((imgObject: ImageType) => {
+    setImages((prevImagesArray) => {
+      if (prevImagesArray) {
+        const filteredImages = prevImagesArray.filter(
+          (item) => item.color === imgObject.color
+        );
+        return filteredImages;
+      }
+      return prevImagesArray;
+    });
+  }, []);
 
   return (
     <>
@@ -86,7 +241,7 @@ const AddProductForm = ({ currentUser }: AddProductFormProps) => {
         register={register}
         label="This Product is in stock"
       />
-
+      {/* select category */}
       <div className="w-full font-medium">
         <div className="mb-2 font-semibold">Select a category</div>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-2 max-h-[50vh] overflow-y-auto">
@@ -108,6 +263,43 @@ const AddProductForm = ({ currentUser }: AddProductFormProps) => {
           })}
         </div>
       </div>
+      {/* select product color and image */}
+      <div
+        className="
+         flex
+         flex-col
+         flex-wrap
+         gap-4
+       "
+      >
+        <div>
+          <div className="font-bold">
+            Select the available product colors and upload their images.
+          </div>
+          <div className="text-sm">
+            You must upload an image for each of the color selected otherwise
+            your color selection will be ignored
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-3">
+          {colors.map((item, index) => {
+            return (
+              <SelectColor
+                key={index}
+                item={item}
+                addImageToState={addImageToState}
+                removeImageFromState={removeImageFromState}
+                isProductCreated={isProductCreated}
+              />
+            );
+          })}
+        </div>
+      </div>
+
+      <Button
+        label={isLoading ? "Loading..." : "Add Product"}
+        onClick={handleSubmit(onsubmitData)}
+      />
     </>
   );
 };
