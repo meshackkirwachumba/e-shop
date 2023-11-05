e388598e7a25b7f3c95c963d1425a8f47d722c87
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-05 20:38:51 +0300

    orders management

--- app/admin/manage-orders/page.tsx
+++ app/admin/manage-orders/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import ManageOrdersClient from "./ManageOrdersClient";
+import Container from "@/app/components/Container";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+import getOrders from "@/actions/getOrders";
 
-const ManageOrders = () => {
-  return <div>ManageOrders</div>;
+const ManageOrders = async () => {
+  const orders = await getOrders();
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Oops! Access Denied" />;
+  }
+  return (
+    <div className="pt-8">
+      <Container>
+        <ManageOrdersClient orders={orders} />
+      </Container>
+    </div>
+  );
 };
 
 export default ManageOrders;
